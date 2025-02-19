dcacffddd7d6727b4757fcf94f1af5333f231ece
xterminator24 <scott.mckillop@outlook.com>
2025-02-19 18:01:48 -0400

    Add weather fetching functionality and create Weather class

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+from weather import Weather
+
 def prompt_for_icao():
     airport_icao = input("ICAO: ")
     return airport_icao.upper()
 
 def main():
     airport_icao = prompt_for_icao()
-    print(f"airport: {airport_icao}")
+    weather = Weather()
+    json = weather.get(airport_icao)
+    print(json)
 
 if __name__ == "__main__":
     main()
---
+++ weather.py
@@ -0,0 +1,17 @@
+import requests
+
+class Weather:
+
+    def __init__(self):
+        self.api_base_url = "https://aviationweather.gov/api/data/metar?"
+
+    def get(self, icao):
+        url = self.api_base_url + f"ids={icao}" + "&format=json"
+        print(f"url: {url}")
+        response = requests.get(url)
+
+        if response.status_code != 200:
+            return None
+        
+        json_data = response.json()
+        return json_data[0]
