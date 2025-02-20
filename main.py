5481da46884262a3f6809d9e6dfc2bd6b50977dd
xterminator24 <scott.mckillop@outlook.com>
2025-02-19 20:43:42 -0400

    Refactor airport data display in main function to improve readability

--- main.py
+++ main.py
@@ -10,12 +10,20 @@ def main():
 
     if not airport:
         print(f"failed to find airport {airport_icao}")
-        
-    print(f"{airport.name} ({airport.icao}) | {airport.country}")
-    print('=====================================')
+
+    print_airport_to_console(airport)
+
+def print_airport_to_console(airport):
+    print()
+    print()
+    print('=========================================================')    
+    print(f"  {airport.name} ({airport.icao}) | {airport.country}")
+    print('=========================================================')
     print(f"wind: {airport.metar.wind_direction} at {airport.metar.wind_speed}")
     print(f"temperature: {airport.metar.temperature} | dewpoint: {airport.metar.dewpoint}")
     print(f"altimeter: {airport.metar.altimeter}")
+    print()
+    print()
         
 
 if __name__ == "__main__":
